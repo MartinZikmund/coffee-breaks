155ae4b68a26f3213f1927e74abe2987a0d99668
Martin Zikmund <martin@zikmund.dev>
2023-01-07 17:40:38 +0100

    Showing a Dialog in Uno Platform

---
+++ UnoPlatformContentDialog/UnoPlatformContentDialog.Shared/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace UnoPlatformContentDialog;
+
+public sealed partial class MainPage : Page
+{
+    public MainPage()
+    {
+        this.InitializeComponent();
+    }
+
+    public async void Button_Click(object sender, RoutedEventArgs e)
+    {
+        ContentDialog dialog = new()
+        {
+            Title = "Save changes",
+            Content = "Are you sure you want to save?",
+            PrimaryButtonText = "Yes",
+            SecondaryButtonText = "Cancel",
+            DefaultButton = ContentDialogButton.Primary,
+            XamlRoot = this.XamlRoot
+        };
+
+        var result = await dialog.ShowAsync();
+
+        if (result == ContentDialogResult.Primary)
+        {
+            // User clicked the primary button.
+            // Save changes.
+        }
+        else
+        {
+            // Revert changes, etc.
+        }
+    }
+}
