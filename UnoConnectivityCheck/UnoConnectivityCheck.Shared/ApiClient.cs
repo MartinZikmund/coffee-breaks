3533662b42b2f00a3a77339d4e5d12fd153e58ce
Martin Zikmund <martin@zikmund.dev>
2023-01-12 14:35:00 +0100

    Uno Platform Connectivity Check

---
+++ UnoConnectivityCheck/UnoConnectivityCheck.Shared/ApiClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace UnoConnectivityCheck
+{
+    internal class ApiClient
+    {
+        private readonly HttpClient _client = new();
+
+        public async Task<string> GetDataAsync()
+        {
+            var response = await _client.GetAsync("https://run.mocky.io/v3/31b38cff-bbe8-4c53-853d-7978422ca3b5");
+            if (response.IsSuccessStatusCode)
+            {
+                return DateTimeOffset.Now.ToString("HH:mm:ss");
+            }
+            else
+            {
+                return "Exception: API is unreachable";
+            }
+        }
+    }
+}
---
+++ UnoConnectivityCheck/UnoConnectivityCheck.Shared/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Networking.Connectivity;
+
+namespace UnoConnectivityCheck;
+
+public sealed partial class MainPage : Page
+{
+    private readonly ApiClient _apiClient = new();
+
+    public MainPage()
+    {
+        this.InitializeComponent();
+    }
+
+    protected override void OnNavigatedTo(NavigationEventArgs e)
+    {
+        base.OnNavigatedTo(e);
+        NetworkInformation.NetworkStatusChanged += NetworkInformation_NetworkStatusChanged;
+        UpdateState();
+    }
+
+    protected override void OnNavigatedFrom(NavigationEventArgs e)
+    {
+        base.OnNavigatedFrom(e);
+        NetworkInformation.NetworkStatusChanged -= NetworkInformation_NetworkStatusChanged;
+    }
+
+    private void NetworkInformation_NetworkStatusChanged(object sender)
+    {
+        UpdateState();
+    }
+
+    private void UpdateState()
+    {
+        DispatcherQueue.TryEnqueue(() =>
+        {
+            var profile = NetworkInformation.GetInternetConnectionProfile();
+            var level = profile?.GetNetworkConnectivityLevel() ?? NetworkConnectivityLevel.None;
+            RefreshButton.IsEnabled =
+                level == NetworkConnectivityLevel.InternetAccess ||
+                level == NetworkConnectivityLevel.ConstrainedInternetAccess;
+        });
+    }
+
+    public async void Refresh()
+    {
+        TimeTextBlock.Text = await _apiClient.GetDataAsync();
+    }
+}
