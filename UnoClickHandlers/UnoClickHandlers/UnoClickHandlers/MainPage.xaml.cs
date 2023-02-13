2a258f98e92d4e36185d96ca77e7e07edafae142
Martin Zikmund <martin@zikmund.dev>
2023-02-13 13:46:11 +0100

    Button Click handling

---
+++ UnoClickHandlers/UnoClickHandlers/UnoClickHandlers/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace UnoClickHandlers;
+
+public sealed partial class MainPage : Page
+{
+    private MainViewModel _viewModel;
+
+    public MainPage() => InitializeComponent();
+
+    internal MainViewModel ViewModel => _viewModel ??= new MainViewModel(XamlRoot);
+
+    private async void Button_Click(object sender, RoutedEventArgs e)
+    {
+        var contentDialog = new ContentDialog()
+        {
+            XamlRoot = XamlRoot,
+            Title = "Button click sample",
+            Content = "Button clicked",
+            PrimaryButtonText = "OK",
+        };
+
+        await contentDialog.ShowAsync();
+    }
+}
---
+++ UnoClickHandlers/UnoClickHandlers/UnoClickHandlers/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿#nullable enable
+using System;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+
+namespace UnoClickHandlers;
+
+internal class MainViewModel
+{
+    private readonly XamlRoot _xamlRoot;
+    private ICommand? _clickCommand = null;
+
+    public MainViewModel(XamlRoot xamlRoot)
+    {
+        _xamlRoot = xamlRoot;
+    }
+
+    public ICommand ClickCommand => _clickCommand ??= new AsyncRelayCommand(ShowDialogAsync);
+
+    public async void TwoArgClick(object sender, object e) => await ShowDialogAsync();
+
+    public async void OneArgClick(object sender) => await ShowDialogAsync();
+    
+    public async Task NoArgClick() => await ShowDialogAsync();
+
+    private async Task ShowDialogAsync()
+    {
+        await Task.Delay(1000);
+
+        var dialog = new ContentDialog()
+        {
+            XamlRoot = _xamlRoot,
+            Title = "Button click sample",
+            Content = "Button clicked",
+            PrimaryButtonText = "OK",
+        };
+
+        await dialog.ShowAsync();
+    }
+}
